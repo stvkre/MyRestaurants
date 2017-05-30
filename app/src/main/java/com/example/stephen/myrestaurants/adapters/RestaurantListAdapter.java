18b7a2b787733853aa8e5a767195aaf17291e635
stvkre <kiariemburuh@gmail.com>
2017-05-30 17:28:13 +0300

    picasso -added picasso to load images next to restaurant results searched with zip codes

--- app/src/main/java/com/example/stephen/myrestaurants/adapters/RestaurantListAdapter.java
+++ app/src/main/java/com/example/stephen/myrestaurants/adapters/RestaurantListAdapter.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 
 import com.example.stephen.myrestaurants.R;
 import com.example.stephen.myrestaurants.models.Restaurant;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -57,6 +58,7 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
         }
 
         public void bindRestaurant(Restaurant restaurant) {
+            Picasso.with(mContext).load(restaurant.getImageUrl()).into(mRestaurantImageView);
             mNameTextView.setText(restaurant.getName());
             mCategoryTextView.setText(restaurant.getCategories().get(0));
             mRatingTextView.setText("Rating: " + restaurant.getRating() + "/5");
